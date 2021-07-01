4d22de874ab6734f0f5690085f47f62cc33df846
00kang <kdb070970@gamil.com>
2021-07-01 10:28:19 +0900

    07/01 commit

--- gisweb_1/settings.py
+++ gisweb_1/settings.py
@@ -17,7 +17,7 @@ import osl as osl
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-local_env = open(os.path.join(BASE_DIR, '..env'))
+local_env = open(os.path.join(BASE_DIR, '.env'))
 
 env_list = dict()
 while True:
@@ -35,7 +35,7 @@ while True:
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list('SECRET_KEY')
+SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
